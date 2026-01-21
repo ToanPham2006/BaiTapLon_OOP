d32334fdfa11857a2c51d0c878b9871b5d1f8103
ToanPham2006 <tp6931211@gmail.com>
2026-01-22 01:55:50 +0700

    hoan thien

---
+++ Library.cpp
@@ -0,0 +1,193 @@
+#include "Library.h"
+#include <iostream>
+#include <fstream>
+#include <iomanip>
+using namespace std;
+Library::Library():SoLuongSach(0),SoLuongSV(0),SoLuongMuon(0){
+ docFile();
+}
+int Library::timSachTheoMa(int ma){
+ for(int i=0;i<SoLuongSach;i++){
+  if(dsSach[i].getma()==ma){
+   return i;
+  }
+ }
+ return -1;
+}
+int Library::timSVTheoMa(int ma){
+ for(int i=0;i<SoLuongSV;i++){
+  if(dsSV[i].getma()==ma){
+   return i;
+  }
+ }
+ return -1;
+}
+bool Library::sachDangMuon(int maSach){
+ for(int i=0;i<SoLuongMuon;i++){
+  if(dsMuon[i].getmasach()==maSach){
+   return true;
+  }
+ }
+ return false;
+}
+bool Library::svDangMuon(int maSV){
+ for(int i=0;i<SoLuongMuon;i++){
+  if(dsMuon[i].getmaSV()==maSV){
+   return true;
+  }
+ }
+ return false;
+}
+void Library::themSach(){
+ dsSach[SoLuongSach].nhap();
+ SoLuongSach++;
+ ghiFile();
+}
+void Library::xoaSach(){
+ int ma;
+ cout<<"Nhap ma sach can xoa: ";
+ cin>>ma;
+ int vitri=timSachTheoMa(ma);
+ if(vitri==-1){
+  cout<<"Khong tim thay sach!"<<endl;
+  return;
+ }
+ if(sachDangMuon(ma)){
+  cout<<"Loi: Sach dang co sinh vien muon, khong the xoa!"<<endl;
+ }
+ else{
+  for(int i=vitri;i<SoLuongSach-1;i++) dsSach[i]=dsSach[i+1];
+  SoLuongSach--;
+  ghiFile();
+  cout<<"Xoa thanh cong!"<<endl;
+ }
+}
+void Library::hienThiSach(){
+ cout<<"\n--- DANH SACH SACH ---\n";
+ for(int i=0;i<SoLuongSach;i++) dsSach[i].xuat();
+}
+void Library::themSV(){
+ dsSV[SoLuongSV].nhap();
+ SoLuongSV++;
+ ghiFile();
+}
+void Library::xoaSV(){
+ int ma;
+ cout<<"Nhap ma SV can xoa: ";
+ cin>>ma;
+ int vitri=timSVTheoMa(ma);
+ if(vitri==-1){
+  cout<<"Khong tim thay sinh vien!"<<endl;
+  return;
+ }
+ if(svDangMuon(ma)){
+  cout<<"Loi: Sinh vien dang muon sach, khong the xoa!"<<endl;
+ }
+ else{
+  for(int i=vitri;i<SoLuongSV-1;i++) dsSV[i]=dsSV[i+1];
+  SoLuongSV--;
+  ghiFile();
+  cout<<"Xoa thanh cong!"<<endl;
+ }
+}
+void Library::muonSach(){
+ int msv,ms,d,m,y;
+ cout<<"Nhap ma SV: ";
+ cin>>msv;
+ cout<<"Nhap ma sach: ";
+ cin>>ms;
+ int viTriSV=timSVTheoMa(msv);
+ int viTriS=timSachTheoMa(ms);
+ if(viTriSV==-1||viTriS==-1){
+  cout<<"Ma SV hoac Ma Sach khong ton tai!"<<endl;
+  return;
+ }
+ if(dsSach[viTriS].gettinhtrang()==1){
+  cout<<"Sach nay da co nguoi muon!"<<endl;
+  return;
+ }
+ cout<<"Ngay muon (d m y): ";
+ cin>>d>>m>>y;
+ dsMuon[SoLuongMuon]=Borrow(msv,ms,d,m,y);
+ SoLuongMuon++;
+ dsSach[viTriS].settinhtrang(1);
+ ghiFile();
+ cout<<"Muon sach thanh cong!"<<endl;
+}
+void Library::traSach(){
+ int ms;
+ cout<<"Nhap ma sach tra: ";
+ cin>>ms;
+ int viTriS=timSachTheoMa(ms);
+ if(viTriS==-1||dsSach[viTriS].gettinhtrang()==0){
+  cout<<"Sach khong o trang thai dang muon!"<<endl;
+  return;
+ }
+ for(int i=0;i<SoLuongMuon;i++){
+  if(dsMuon[i].getmasach()==ms){
+   for(int j=i;j<SoLuongMuon-1;j++) dsMuon[j]=dsMuon[j+1];
+   SoLuongMuon--;
+   break;
+  }
+ }
+ dsSach[viTriS].settinhtrang(0);
+ ghiFile();
+ cout<<"Tra sach thanh cong!"<<endl;
+}
+void Library::thongKeMuon(){
+ cout<<"\n--- THONG KE MUON SACH ---\n";
+ for(int i=0;i<SoLuongMuon;i++){
+  int viTriSV=timSVTheoMa(dsMuon[i].getmaSV());
+  int viTriS=timSachTheoMa(dsMuon[i].getmasach());
+  cout<<"SV: ";
+  if(viTriSV>=0){
+   cout<<dsSV[viTriSV].getten();
+  }
+  else{
+   cout<<"Khong tim thay sinh vien";
+  }
+  cout<<" | Sach: ";
+  if(viTriS>=0){
+   cout<<dsSach[viTriS].getten();
+  }
+  else{
+   cout<<"Khong tim thay sach";
+  }
+  cout<<" | ";
+  dsMuon[i].xuat();
+ }
+}
+void Library::ghiFile(){
+ ofstream fS("sach.dat",ios::binary);
+ fS.write((char*)&SoLuongSach,sizeof(int));
+ for(int i=0;i<SoLuongSach;i++) dsSach[i].WriteBinary(fS);
+ fS.close();
+ ofstream fSV("sv.dat",ios::binary);
+ fSV.write((char*)&SoLuongSV,sizeof(int));
+ for(int i=0;i<SoLuongSV;i++) dsSV[i].WriteBinary(fSV);
+ fSV.close();
+ ofstream fM("muon.dat",ios::binary);
+ fM.write((char*)&SoLuongMuon,sizeof(int));
+ for(int i=0;i<SoLuongMuon;i++) dsMuon[i].WriteBinary(fM);
+ fM.close();
+}
+void Library::docFile(){
+ ifstream fS("sach.dat",ios::binary);
+ if(fS){
+  fS.read((char*)&SoLuongSach,sizeof(int));
+  for(int i=0;i<SoLuongSach;i++) dsSach[i].ReadBinary(fS);
+  fS.close();
+ }
+ ifstream fSV("sv.dat",ios::binary);
+ if(fSV){
+  fSV.read((char*)&SoLuongSV,sizeof(int));
+  for(int i=0;i<SoLuongSV;i++) dsSV[i].ReadBinary(fSV);
+  fSV.close();
+ }
+ ifstream fM("muon.dat",ios::binary);
+ if(fM){
+  fM.read((char*)&SoLuongMuon,sizeof(int));
+  for(int i=0;i<SoLuongMuon;i++) dsMuon[i].ReadBinary(fM);
+  fM.close();
+ }
+}
---
+++ main.cpp
@@ -0,0 +1,69 @@
+#include <iostream>
+#include "Library.h"
+using namespace std;
+void hienThiMenu(){
+ cout<<"\n================ QUAN LY THU VIEN ================"<<endl;
+ cout<<"1. Nhap them 1 cuon sach"<<endl;
+ cout<<"2. Sua thong tin sach (theo ma)"<<endl;
+ cout<<"3. Xoa 1 cuon sach (theo ma)"<<endl;
+ cout<<"4. Nhap thong tin sinh vien"<<endl;
+ cout<<"5. Sua thong tin sinh vien (theo ma)"<<endl;
+ cout<<"6. Xoa thong tin sinh vien (theo ma)"<<endl;
+ cout<<"7. Muon sach"<<endl;
+ cout<<"8. Tra sach"<<endl;
+ cout<<"9. Liet ke toan bo sach trong thu vien"<<endl;
+ cout<<"10. Liet ke sach duoc muon cua 1 sinh vien cu the"<<endl;
+ cout<<"11. Liet ke toan bo sach dang duoc muon"<<endl;
+ cout<<"12. Thoat khoi he thong"<<endl;
+ cout<<"--------------------------------------------------"<<endl;
+ cout<<"Lua chon cua ban: ";
+}
+int main(){
+ Library lib;
+ int choice;
+ do{
+  hienThiMenu();
+  cin>>choice;
+  switch(choice){
+   case 1:
+    lib.themSach();
+    break;
+   case 2:
+    cout<<"Chuc nang dang duoc cap nhat"<<endl;
+    break;
+   case 3:
+    lib.xoaSach();
+    break;
+   case 4:
+    lib.themSV();
+    break;
+   case 5:
+    cout<<"Chuc nang dang duoc cap nhat"<<endl;
+    break;
+   case 6:
+    lib.xoaSV();
+    break;
+   case 7:
+    lib.muonSach();
+    break;
+   case 8:
+    lib.traSach();
+    break;
+   case 9:
+    lib.hienThiSach();
+    break;
+   case 10:
+    cout<<"Chuc nang dang duoc cap nhat..."<<endl;
+    break;
+   case 11:
+    lib.thongKeMuon();
+    break;
+   case 12:
+    cout<<"Cam on ban da su dung he thong. Tam biet!"<<endl;
+    break;
+   default:
+    cout<<"Lua chon khong hop le, vui long nhap lai!"<<endl;
+  }
+ }while(choice!=12);
+ return 0;
+}
